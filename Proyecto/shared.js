8c8408afad6ab430aef4a8f190decdae02e69250
Jonathan Lasso <jonathan.lasso30@gmail.com>
2025-12-09 18:16:41 -0500

    Corregí algunos errores al momento de expirar los datos al localstorage

--- Proyecto/shared.js
+++ Proyecto/shared.js
@@ -9,18 +9,19 @@
         //Plantillas HTML (vistas)
         const templates = {
             tarjetaPokemon: (pokemon) => {
+                //para el tipos de pokemon
                 const tiposHtml = pokemon.types.map(tipo => `<span class="tipo-insignia">${tipo.type.name}</span>`).join('')
-
+                //habilidades del pokemon
                 const habilidadesHtml = pokemon.abilities.map(habilidad => {
                     const claseHabilidad = habilidad.is_hidden ? 'habilidad-oculta' : 'habilidad-normal';
                     return `<span class="${claseHabilidad}">${habilidad.ability.name}</span>`}).join('')
-
+                //Estadisticas del pokemon
                 const estadisticasHtml = pokemon.stats.map(estadisticas => `
                 <div class="etiqueta-estadisticas">${estadisticas.stat.name.toUpperCase()}:</div>
                 <div class="contenedor-barra-estadisticas">
                     <div class="barra-estadisticas" style="width: ${Math.min(estadisticas.base_stat, 100)}%"></div>
                 </div>`).join('')
-
+                //Para verificar si viene del cache o API
                 const cacheBadge = pokemon.esCache ? 'cache' : 'api';
 
                 return `
@@ -65,6 +66,7 @@
                 let historial = JSON.parse(localStorage.getItem(key)) || [];
                 // Verificación si hay busquedas repetidas
                 if (!historial.some(p=> p.id === pokemon.id || p.name === pokemon.name)) {
+                    //creamos un historial con tiempo
                     const historialConTiempo = {
                         ...pokemon,
                         timestamp: fechaAhoraActual,
@@ -72,6 +74,7 @@
                     historial.push(historialConTiempo);
                     // Guardar
                     localStorage.setItem(key, JSON.stringify(historial));
+                    //Mostramos si se guardo correctamente
                     historial.forEach((historial) => {
                         console.log((`Pokemon: ${pokemon.name} en localStorage con tiempo:" ${new Date(fechaAhoraActual)}`))
                     })
@@ -79,19 +82,21 @@
                 }
             },
             obtenerHistorial() {
-                const HORAS_24_EN_MS = 24 * 60 * 60 * 1000;
+                //const Horas24EnMS = 24 * 60 * 60 * 1000;//Duración de expiración de 24 horas
+                const CincoMinutosEnMS = 5 * 60 * 1000;// Duración de expiración de 5 minutos
                 const fechaAhoraActual = Date.now();
                 let historial = JSON.parse(localStorage.getItem('historialPokemon')) || [];
 
                 // Filtramos: Solo sobreviven los que (Ahora - Creado) sea MENOR a 24 horas
                 const historialActualizado = historial.filter(pokemon => {
                     const tiempoTrancurrido = fechaAhoraActual - pokemon.timestamp;
-                    return tiempoTrancurrido < HORAS_24_EN_MS;
+                    return tiempoTrancurrido < CincoMinutosEnMS;
                 })
                 // Si la cantidad de elementos cambió, significa que algunos expiraron y debemos actualizar el localStorage
                 if(historial.length !== historialActualizado.length){
                     console.log("¡Se han borrado búsquedas expiradas del localStorage!")
                     localStorage.setItem('historialPokemon', JSON.stringify(historialActualizado));
+                    return historialActualizado
                 }
                 else {
                     return historial;
@@ -101,7 +106,7 @@
                 let historial = JSON.parse(localStorage.getItem('historialPokemon')) || [];
                 const pokemonEncontrado = historial.find(p=> p.id == busqueda || p.name === busqueda.toLowerCase());
                 if(pokemonEncontrado) {
-                    pokemonEncontrado.esCache = true;
+                    pokemonEncontrado.esCache = true; //creamos una propieda llamada esCache para verificar que si esta en el cache
                     htmlElements.contenedorPokemon.innerHTML = templates.tarjetaPokemon(pokemonEncontrado);
                     console.log("ya esta en el historial");
                     return true;
@@ -142,12 +147,16 @@
             init() {
                 htmlElements.formulario.addEventListener('submit', handlers.alHacerClickBuscar);
                 const historial = utils.obtenerHistorial()
-                if(historial.length > 0) {
-                    document.addEventListener("DOMContentLoaded", () => {
-                        historial.forEach(pokemon => {
-                            console.log(pokemon.id,pokemon.name, new Date(pokemon.timestamp).toLocaleString());
-                        })
-                    })
+                if (historial.length === 0) {
+                    console.log("El historial está vacío");
+                } else {
+                    historial.forEach(pokemon => {
+                        console.log(
+                            pokemon.id,
+                            pokemon.name,
+                            new Date(pokemon.timestamp).toLocaleString()
+                        );
+                    });
                 }
             }
         };
